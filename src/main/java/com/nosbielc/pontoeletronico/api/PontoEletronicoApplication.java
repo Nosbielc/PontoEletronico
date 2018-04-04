8492c261ae3fcfc0c6e7b408ea444ca6b90bbcac
Cleibson Gomes da Silva <cleibson.gomes.silva@gmail.com>
2018-04-04 15:02:53 -0300

    Adicionado suporte ao CI com TravisCI

---
+++ src/main/java/com/nosbielc/pontoeletronico/api/PontoEletronicoApplication.java
@@ -0,0 +1,12 @@
+package com.nosbielc.pontoeletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletronicoApplication.class, args);
+	}
+}
