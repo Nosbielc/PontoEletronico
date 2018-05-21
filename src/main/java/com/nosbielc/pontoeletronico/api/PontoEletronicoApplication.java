bee976656e58d4cef8ada69f3ea874b374f724f3
Cleibson Gomes <cleibson.gomes.silva@gmail.com>
2018-05-21 00:36:34 -0300

    Adicionando EhCache

--- src/main/java/com/nosbielc/pontoeletronico/api/PontoEletronicoApplication.java
+++ src/main/java/com/nosbielc/pontoeletronico/api/PontoEletronicoApplication.java
@@ -2,8 +2,10 @@ package com.nosbielc.pontoeletronico.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PontoEletronicoApplication {
 
 	public static void main(String[] args) {
